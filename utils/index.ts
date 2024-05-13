694ffa3a44417fe5b1d977aac8dec42e7e4351fb
Jun Wu <jun@flouriship.com>
2024-05-12 23:10:42 -0700

    change to use lanchain

--- utils/index.ts
+++ utils/index.ts
@@ -1,64 +1,28 @@
-import { Message, OpenAIModel } from "@/types";
-import { createParser, ParsedEvent, ReconnectInterval } from "eventsource-parser";
+import { Message } from "@/types";
+import { ChatOpenAI } from "@langchain/openai";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+
+const model = new ChatOpenAI({
+  model: "gpt-4-turbo-2024-04-09",
+  temperature: 0.9,
+  openAIApiKey: process.env.OPENAI_API_KEY
+});
+
+
 
 export const OpenAIStream = async (messages: Message[]) => {
-  const encoder = new TextEncoder();
-  const decoder = new TextDecoder();
+  
 
-  const res = await fetch("https://api.openai.com/v1/chat/completions", {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
+  let inputMsg = JSON.stringify( [
+    {
+      role: "system",
+      content: "You are a helpful, friendly, assistant."
     },
-    method: "POST",
-    body: JSON.stringify({
-      model: OpenAIModel.DAVINCI_TURBO,
-      messages: [
-        {
-          role: "system",
-          content: `You are a helpful, friendly, assistant.`
-        },
-        ...messages
-      ],
-      max_tokens: 800,
-      temperature: 0.0,
-      stream: true
-    })
-  });
+    ...messages
+  ])
 
-  if (res.status !== 200) {
-    throw new Error("OpenAI API returned an error");
-  }
-
-  const stream = new ReadableStream({
-    async start(controller) {
-      const onParse = (event: ParsedEvent | ReconnectInterval) => {
-        if (event.type === "event") {
-          const data = event.data;
-
-          if (data === "[DONE]") {
-            controller.close();
-            return;
-          }
-
-          try {
-            const json = JSON.parse(data);
-            const text = json.choices[0].delta.content;
-            const queue = encoder.encode(text);
-            controller.enqueue(queue);
-          } catch (e) {
-            controller.error(e);
-          }
-        }
-      };
-
-      const parser = createParser(onParse);
-
-      for await (const chunk of res.body as any) {
-        parser.feed(decoder.decode(chunk));
-      }
-    }
-  });
-
+  // Conver to Stream.
+  const parser = new StringOutputParser();
+  const stream = await model.pipe(parser).stream(inputMsg);
   return stream;
 };
